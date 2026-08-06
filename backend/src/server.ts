0e1153141a2c8ae6555a253d94779a52fcc2a4ca
n1cqtinx <n1cqtinx@gmail.com>
2026-08-06 17:51:16 +0500

    Basic container-based server with nginx and two node servers

---
+++ backend/src/server.ts
@@ -0,0 +1,49 @@
+import http from "node:http";
+import os from "node:os";
+
+const PORT = 3000;
+
+const server = http.createServer(
+    (req, res) => {
+
+        console.log(req.method);
+        console.log(req.url);
+
+        if (
+            req.url === "/api/server" &&
+            req.method === "GET"
+        ) {
+
+            res.writeHead(200, {
+                "Content-Type": "application/json"
+            });
+
+
+            res.end(
+                JSON.stringify({
+                    server: os.hostname()
+                })
+            );
+
+
+            return;
+        }
+
+
+
+        res.writeHead(404);
+
+        res.end("Not found");
+
+    }
+);
+
+
+
+server.listen(PORT, "0.0.0.0", () => {
+
+    console.log(
+        `Server started on ${PORT}`
+    );
+
+});
